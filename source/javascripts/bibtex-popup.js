dbde2adebe52e5da392713b4985f4d66a47b1901
Joseph Chang <bizkit.tw@gmail.com>
2012-11-22 18:21:52 +0800

    IE8 SUPPORT.

--- source/javascripts/bibtex-popup.js
+++ source/javascripts/bibtex-popup.js
@@ -1,24 +1,24 @@
 function bindBibtexQtips() {
-	jQuery('.bibtex').each(function(){
-		jQuery(this).qtip({
-			content: jQuery(this).attr("bibtex"),
-			show: 'click',
-			hide: { when: { event: 'unfocus' } },
+ jQuery('.bibtex').each(function(){
+  jQuery(this).qtip({
+   content: jQuery(this).attr("bibtex"),
+   show: 'click',
+   hide: { when: { event: 'unfocus' } },
 
-			
-			style: {
-				width: 600,
-				name: 'green',
-				tip: 'topLeft',
-			},
 
-			position: {
-				corner: {
+   style: {
+    width: 600,
+    name: 'green',
+    tip: 'topLeft'
+   },
+
+   position: {
+    corner: {
                     target: 'topRight',
                     tooltip: 'leftTop'
-				}
-			}
-		});
-	});
+    }
+   }
+  });
+ });
 
 }
