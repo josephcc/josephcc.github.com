38b399815dfaf19ff44315f4929e8c843ac65eca
Joseph Chee Chang <josephcc@cmu.edu>
2021-07-29 10:16:10 -0400

    Site updated at 2021-07-29 14:16:10 UTC

---
+++ _deploy/javascripts/generate-toc.js
@@ -0,0 +1,16 @@
+function generateTOC(insertHead, target, heading) {
+  var container = jQuery("<section id='tocBlock'></section>");
+  var div = jQuery("<ul id='toc'></ul>");
+  var content = $(target).first();
+
+  if (heading != undefined && heading != null) {
+    container.append('<h1>' + heading + '</h1>');
+  }
+
+  div.tableOfContents(content);
+  div = div.children('li').first().children('ul').first();
+  div2 = jQuery("<ol></ol>")
+  div2.append(div.html());
+  container.append(div2);
+  $(insertHead).prepend(container);
+}
